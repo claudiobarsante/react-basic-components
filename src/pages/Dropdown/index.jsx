e8143b1db9f7d0457b71105ae26e814da318ac6d
Claudio Barsante <clbmribas@gmail.com>
2021-04-11 21:20:39 -0300

    Created InputText folder

--- src/pages/Dropdown/index.jsx
+++ src/pages/Dropdown/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import './styles.css';
 export default function Dropdown() {
 	const [valSelect, setValSelect] = useState({ optionValue: '', optionText: '' });
 
@@ -12,20 +12,35 @@ export default function Dropdown() {
 	};
 
 	return (
-		<div>
+		<div className='select-container'>
 			<form>
+				<h2>Dropdown List</h2>
 				<fieldset>
-					<div className='select-container'>
-						<h2>Dropdown List</h2>
-						<select onChange={handleChangeSelect}>
+					<label for='simple'>Simple dropdown</label>
+					<select id='simple' onChange={handleChangeSelect}>
+						<option value=''>Select one item...</option>
+						<option value='1'>First item</option>
+						<option value='2'>Second item</option>
+						<option value='3'>Third item</option>
+					</select>
+					<p>Selected value = {valSelect.optionValue}</p>
+					<p>Corresponding text = {valSelect.optionText}</p>
+					<label for='group'>Group dropdown</label>
+					<select id='simple' onChange={handleChangeSelect}>
+						<optgroup label='Group one'>
 							<option value=''>Select one item...</option>
 							<option value='1'>First item</option>
 							<option value='2'>Second item</option>
 							<option value='3'>Third item</option>
-						</select>
-						<p>Selected value = {valSelect.optionValue}</p>
-						<p>Corresponding text = {valSelect.optionText}</p>
-					</div>
+						</optgroup>
+						<optgroup label='Group two'>
+							<option value='4'>First item - group2</option>
+							<option value='5'>Second item - group2</option>
+							<option value='6'>Third item - group2</option>
+						</optgroup>
+					</select>
+					<p>Selected value = {valSelect.optionValue}</p>
+					<p>Corresponding text = {valSelect.optionText}</p>
 				</fieldset>
 			</form>
 		</div>
